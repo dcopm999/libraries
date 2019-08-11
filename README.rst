7f5a84f55567b299004d93b78026a69a52c66aa5
Pavel Tanchev <dcopm999@gmail.com>
2019-08-11 13:55:14 +0500

    Добавил setup.py и README.rst

---
+++ README.rst
@@ -0,0 +1,16 @@
+=====
+PharmCRM libraries
+=====
+
+Набор базовых библиотке для PharmCRM
+
+Quick start
+-----------
+
+1. Add "libraries" to your INSTALLED_APPS setting like this::
+
+     INSTALLED_APPS = (
+     ...
+     'libraries',
+     )
+     
---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+    os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+    setup(
+        name='PharmCRM-libraries',
+        version='0.1',
+        packages=['libraries'],
+        include_package_data=True,
+        license='BSD License',
+        description='Набор стандартных библиотек для PharmCRM',
+        long_description=README,
+        url='http://crm.atm.uz/',
+        author='Pavel Tanchev',
+        author_email='dcopm999@gmail.com',
+        classifiers=[
+            'Environment :: Web Environment',
+            'Framework :: Django',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Topic :: Internet :: WWW/HTTP',
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        ],
+    )

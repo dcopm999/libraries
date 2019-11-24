98d0df9ebf5334a1e959b8be9ce6744ac74693fc
Pavel Tanchev <dcopm999@gmail.com>
2019-11-24 21:57:56 +0500

    admin.py: Исправление замечаний pylint

--- libraries/admin.py
+++ libraries/admin.py
@@ -1,3 +1,7 @@
+'''
+package: libraries
+description: Модуль административной панели
+'''
 from django.contrib import admin
 
 from libraries import models
@@ -5,4 +9,7 @@ from libraries import models
 
 @admin.register(models.Registration)
 class RegistrationAdmin(admin.ModelAdmin):
+    '''
+    Админка для параметров подключения к CoreAPI
+    '''
     list_display = ['token']

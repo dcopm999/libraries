95d40730ef10d096de6f4e365a9ed56a67da8e70
Pavel Tanchev <dcopm999@gmail.com>
2019-11-24 21:56:10 +0500

    managers.py: Исправление замечаний pyliny

--- libraries/managers.py
+++ libraries/managers.py
@@ -1,7 +1,12 @@
+'''
+package: libraries
+description: Модуль с менеджерами взаимодействия с CoreAPI
+'''
 from libraries.base import BaseManager
-
-from libraries.utils import get_token
 
 
 class Manager(BaseManager):
+    '''
+    Менеджер управления записями country в CoreAPI
+    '''
     URI = '/api/address/country/'
